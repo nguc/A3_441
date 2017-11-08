31a588ab2ce744584df9514b9b2d96cb04c63354
nguc <c.ngu.91@gmail.com>
2017-11-07 18:36:54 -0700

    Starting code

---
+++ FastFtp.java
@@ -0,0 +1,66 @@
+
+/**
+ * FastFtp Class
+ *
+ */
+
+import cpsc441.a3.shared.*;
+
+public class FastFtp {
+
+	/**
+     * Constructor to initialize the program 
+     * 
+     * @param windowSize	Size of the window for Go-Back_N in terms of segments
+     * @param rtoTimer		The time-out interval for the retransmission timer
+     */
+	public FastFtp(int windowSize, int rtoTimer) {
+		// to be completed
+	}
+	
+
+    /**
+     * Sends the specified file to the specified destination host:
+     * 1. send file/connection infor over TCP
+     * 2. start receving thread to process coming ACKs
+     * 3. send file segment by segment
+     * 4. wait until transmit queue is empty, i.e., all segments are ACKed
+     * 5. clean up (cancel timer, interrupt receving thread, close sockets/files)
+     * 
+     * @param serverName	Name of the remote server
+     * @param serverPort	Port number of the remote server
+     * @param fileName		Name of the file to be trasferred to the rmeote server
+     */
+	public void send(String serverName, int serverPort, String fileName) {
+		// to be completed
+	}
+	
+	
+    /**
+     * A simple test driver
+     * 
+     */
+	public static void main(String[] args) {
+		// all srguments should be provided
+		// as described in the assignment description 
+		if (args.length != 5) {
+			System.out.println("incorrect usage, try again.");
+			System.out.println("usage: FastFtp server port file window timeout");
+			System.exit(1);
+		}
+		
+		// parse the command line arguments
+		// assume no errors
+		String serverName = args[0];
+		int serverPort = Integer.parseInt(args[1]);
+		String fileName = args[2];
+		int windowSize = Integer.parseInt(args[3]);
+		int timeout = Integer.parseInt(args[4]);
+
+		// send the file to server
+		FastFtp ftp = new FastFtp(windowSize, timeout);
+		System.out.printf("sending file \'%s\' to server...\n", fileName);
+		ftp.send(serverName, serverPort, fileName);
+		System.out.println("file transfer completed.");
+	}
+}
